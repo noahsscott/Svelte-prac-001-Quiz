6264a52af849701ccbcfa3098b327f96aef16bf4
Noah Scott <noahsscott@gmail.com>
2023-08-06 22:54:16 +0800

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Quiz from './Quiz.svelte'; // Same import syntax as JS
+</script>
+
+<main>
+	<h1>Hello you !</h1>
+	<Quiz />
+</main>
+
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+	}
+</style>
---
+++ src/Quiz.svelte
@@ -0,0 +1,8 @@
+<script>
+  export const quizName = "Noah " + " Quiz"
+</script>
+
+
+<div>
+  <h2>{quizName}</h2>
+</div>
